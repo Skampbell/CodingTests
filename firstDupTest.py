898df8ab7cc1ca74ee5be6d1eb374d4f86cdf72b
CamBit <skyecam7@gmail.com>
2022-02-11 08:36:58 -0800

    Create firstDupTest.py

---
+++ firstDupTest.py
@@ -0,0 +1,42 @@
+"""
+Given an array a that contains only numbers in the range from 1 to a.length,
+find the first duplicate number for which the second occurrence has the minimal index.
+In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a
+smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
+
+Example
+
+For a = [2, 1, 3, 5, 3, 2], the output should be solution(a) = 3.
+
+There are 2 duplicates: numbers 2 and 3. The second occurrence of 3 has a smaller index than the second occurrence of 2 does, so the answer is 3.
+
+For a = [2, 2], the output should be solution(a) = 2;
+
+For a = [2, 4, 3, 5, 1], the output should be solution(a) = -1.
+"""
+
+def getFirstDuplicate(in_arr):
+    set_ = set()
+    for item in in_arr:
+        if item in set_:
+            return item
+        set_.add(item)
+    return -1
+
+
+def test():
+    testArrs = [2, 1, 3, 5, 3, 2],[2, 2],[2, 4, 3, 5, 1],[1], [5, 5, 5, 5, 5]
+    expected = [3, 2, -1, -1, 5]
+
+    for arrNum,testArr in enumerate(testArrs):
+        output = getFirstDuplicate(testArr)
+        if output != expected[arrNum]:
+            print('Failed input: ' + str(testArr))
+            print('Expected = ' + str(expected[arrNum]) + ' != ' + str(output))
+            return False
+
+    print("Passed!")
+    return True
+
+
+test()
