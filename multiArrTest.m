2c98ade75c660d200034c451ecd4b5798b99cb88
CamBit <skyecam7@gmail.com>
2024-03-26 14:59:47 -0700

    Add files via upload

---
+++ multiArrTest.m
@@ -0,0 +1,14 @@
+﻿% (a) Generate a random number k between -30.4 and 12.6.
+% (b) Generate an array A of size 20-by-20 of random integers in the
+%       interval [40, 10]. Subsequently, replace by 0 all elements of A
+%       which are smaller than k.
+% (c) Find the mean of all non-zero elements of A.
+% (d) Pick a random element from A.
+% (e) Visualise A using a random colour map containing exactly as many
+%       colours as there are different elements of A.
+% (f) Extract 4 different random rows from A and save them in a new
+%       array B.
+% (g) Find the proportion of non-zero elements of B.
+% (h) Display in the Command Window the answers of % (a), % (c), % (d) 
+%       and (g) with a proper description of each one.
+
